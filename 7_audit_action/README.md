b828bcc2a63545f709ae8f2211bf700fc10dd4c3
BillWang139967 <wangbin139967@163.com>
2016-08-16 16:25:25 +0800

    add:audit_action

---
+++ 7_audit_action/README.md
@@ -0,0 +1,18 @@
+## 使用[PROMPT_COMMAND]变量 实现审计操作行为功能
+
+下载方式
+```
+#curl -o denyhosts.sh https://raw.githubusercontent.com/BillWang139967/linux_tools/master/7_audit_action/audit_action.sh
+```
+### 提示
+
+此日志文件如果需要删除时，在执行删除操作时，会提示没有权限，这个就是chattr对文件实现的保护功能了，如果需要删除此文件，需要执行
+
+```
+chattr -a /var/log/Command_history.log
+chmod 777 /var/log/Command_history.log
+```
+
+然后对日志文件进行删除操作
+
+
---
+++ 7_audit_action/audit_action.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FILENAME="/var/log/Command_history.log"
+PATHNAME="/etc/profile"
+FINDNAME="HISTORY_FILE"
+if [[ ! -f ${FILENAME} ]]
+then
+    #创建行为审计日志文件
+    touch ${FILENAME}     
+    #将日志文件的所有者改为权限低的用户NOBODY
+    chown nobody:nobody ${FILENAME}         
+    #赋予所有用户对日志文件写的权限
+    chmod 002 ${FILENAME}
+    #使所有用户对日志文件只有追加权限
+    chattr +a ${FILENAME}
+fi 
+
+if [[ `cat ${PATHNAME} | grep ${FINDNAME} | wc -l` < 1 ]]; then
+cat >> ${PATHNAME} <<"EOF"
+export HISTORY_FILE=/var/log/Command_history.log
+export PROMPT_COMMAND='{ date "+%y-%m-%d %T ##### $(who am i |awk "{print \$1\" \"\$2\" \"\$5}")  #### $(history 1 | { read x cmd; echo "$cmd"; })"; } >>${HISTORY_FILE}'
+ulimit -SHn 65535 
+EOF
+else
+      exit 0 
+fi
