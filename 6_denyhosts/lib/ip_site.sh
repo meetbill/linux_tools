91768d8dba6d76cd9e25cf856330c19597ec0beb
meetbill <meetbill@163.com>
2017-08-14 09:55:54 +0800

    update:doc

--- 6_denyhosts/lib/ip_site.sh
+++
@@ -1,9 +0,0 @@
-#########################################################################
-# File Name: ip_site.sh
-# Author: Bill
-# mail: XXXXXXX@qq.com
-# Created Time: 2016-08-06 01:57:57
-#########################################################################
-#!/bin/bash
-
-curl -s "http://ip138.com/ips138.asp?ip=$1&act=2" | iconv -f gb2312 -t utf-8| grep '<ul class="ul1"><li>'| awk -F'[><]+' '{print substr($5,6)}'
--- 6_denyhosts/login.sh
+++
@@ -1,9 +0,0 @@
-#########################################################################
-# File Name: login.sh
-# Author: Bill
-# mail: XXXXXXX@qq.com
-# Created Time: 2016-08-06 01:25:55
-#########################################################################
-#!/bin/bash
-cat /var/log/secure | awk '/Accepted password/{print $0 $(NF-3)}' |awk 'BEGIN{split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec",k," ");for(i=1;i<13;i++){m[k[i]]=i}}{$1=m[$1];print "/"$1"/"$2" "$3" "$(NF-5)" ["$(NF-3)"] ""Password"}'
-cat /var/log/secure | awk '/Accepted publickey/{print $0 $(NF-3)}' |awk 'BEGIN{split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec",k," ");for(i=1;i<13;i++){m[k[i]]=i}}{$1=m[$1];print "/"$1"/"$2" "$3" "$(NF-5)" ["$(NF-3)"] ""Key"}'
